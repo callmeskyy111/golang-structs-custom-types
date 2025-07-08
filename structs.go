c6eaccd34a0f3ec9c7f65c65e9287ac20d07d953
Soumadip <39thdecember@gmail.com>
2025-07-08 15:19:50 +0530

    Mutate methods with Ptr* ✅

--- structs.go
+++ structs.go
@@ -2,16 +2,55 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+// user -> local, User -> for exporting.. Both are ok!
+// Custom-Struct
+type User struct{
+firstName string
+lastName string
+birthDate string
+createdAt time.Time
+}
+
+// Attaching methods to the struct
+// * - Optional to just output
+func(u *User) outPutUserDetails(){
+	//...
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+}
+
+// Always use Pointers(*) to mutate
+func (u *User) clearUserName(){
+	u.firstName= ""
+	u.lastName= ""
+}
+
+
+
 func main() {
-	firstName := getUserData("Please enter your first name: ")
-	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	//var appUser User{} // Empty struct
+
+	//instance of User-struct
+
+	//  var appUser User 
+	var appUser = User{
+		firstName: userFirstName,
+		lastName: userLastName,
+		birthDate: userBirthdate,
+		createdAt: time.Now(),
+	}
 
 	// ... do something awesome with that gathered data!
-
-	fmt.Println(firstName, lastName, birthdate)
+	appUser.outPutUserDetails()
+	appUser.clearUserName()
+	appUser.outPutUserDetails()
+	
 }
 
 func getUserData(promptText string) string {
